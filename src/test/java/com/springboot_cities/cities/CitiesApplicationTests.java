848639e3e6910d9f1c969afa1940debec6677b52
Andriana0121 <vorotna.andriana01@gmail.com>
2021-09-20 12:10:24 +0300

    Initial commit

---
+++ src/test/java/com/springboot_cities/cities/CitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_cities.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
