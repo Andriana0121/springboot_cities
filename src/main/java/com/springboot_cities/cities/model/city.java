a7c683296839dd411ba6d4edcb853ca2a61c1f29
Andriana0121 <vorotna.andriana01@gmail.com>
2021-09-27 08:23:56 +0300

    initial

---
+++ src/main/java/com/springboot_cities/cities/model/city.java
@@ -0,0 +1,65 @@
+package com.springboot_cities.cities.model;
+
+public class city {
+    private Integer id = null;
+    private String name = null;
+
+    private Integer countryId = null;
+    private String countryName;
+    public  static  Integer autoincrementId =  0 ;
+
+    City(String cityName, Integer countryId) {
+
+    }
+
+    public City(String name, Integer countryId, String countryName) {
+        City.autoincrementId++;
+        this.id = City.autoincrementId;
+        this.name = name;
+        this.countryId = countryId;
+        this.countryName = countryName;
+    }
+
+    public  Integer  getId () {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Integer countryId) {
+        this.countryId = Country.autoincrementId;
+    }
+//     todo to implement this method which country the city belongs to
+
+    public String getCountryName(){
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cityId=" + countryId +
+                ", cityName=" + this.getCountryName() +
+                ' } ' ;
+    }
+}
